fd1639a080532f299661db61603166dd33326f6b
Aramis Romero <aramisromero89@gmail.com>
2023-06-29 21:38:51 +0200

    add loader test

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/loaderio-1afbc15d8a2736e3bb43142adbc5f75a', (req, res) => {
+  res.send('loaderio-1afbc15d8a2736e3bb43142adbc5f75a')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
